282b33e5b1e414b2161b3cc7b605f34fdcf0af02
Lydia <lydiazly2@gmail.com>
2026-01-23 12:54:22 -0800

    [no ci] Bump version

--- src/clinictracker/__init__.py
+++ src/clinictracker/__init__.py
@@ -2,7 +2,7 @@
 """Fetches clinic updates across a specified region from a target website."""
 
 __all__ = ['run']
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 __author__ = 'Lydia Zhang'
 
 from clinictracker.core import run
