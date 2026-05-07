866468608eed6bd16f0360e0ff5aa403052117d4
Lydia <lydiazly2@gmail.com>
2026-05-06 17:25:09 -0700

    [no ci] use version()

--- src/clinictracker/__init__.py
+++ src/clinictracker/__init__.py
@@ -1,8 +1,10 @@
 # __init__.py
 """Fetches clinic updates across a specified region from a target website."""
 
+from importlib.metadata import version
+
 __all__ = ['run']
-__version__ = '0.1.5'
+__version__ = version('clinictracker')
 __author__ = 'Lydia Zhang'
 
 from clinictracker.core import run
